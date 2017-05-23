089fe7c35fb5305721d78497ecdb8076d1714586
Maxim.Kozlov <Maxim.Kozlov@MKOZLOV-PC>
2017-05-23 17:46:25 +0300

    arsg lengs output

--- Arbitrage/Arbitrage/Program.cs
+++ Arbitrage/Arbitrage/Program.cs
@@ -7,6 +7,7 @@ namespace Arbitrage
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(args.Length);
             Console.ReadLine();
         }
     }
